b1a6e465b4c44a4a13fd104d5937335181fe49ae
thatshubham <shubhambose@protonmail.com>
2020-01-04 02:04:41 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import praw
+
+reddit = praw.Reddit('bot1')
+
+subreddit = reddit.subreddit("easyplayground")
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
+
+    
